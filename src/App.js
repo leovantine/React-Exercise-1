5c3064de95e8d725cf8f89066e19a9f4b4028052
Leonardo Vantine <93939374+leovantine@users.noreply.github.com>
2022-02-10 18:45:18 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class Nome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Olá, meu nome é Leonardo!</h1>
+      </div>
+    );
+  }
+}
